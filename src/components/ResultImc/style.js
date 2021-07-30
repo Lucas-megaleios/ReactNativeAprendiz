b9eec0ad1dfc871850b50893f81119d4291a95d0
Lucas-megaleios <86004242+Lucas-megaleios@users.noreply.github.com>
2021-07-30 15:57:36 -0300

    Learning about APIs - Part 2

--- src/components/ResultImc/style.js
+++ src/components/ResultImc/style.js
@@ -14,10 +14,26 @@ const styles = StyleSheet.create({
     color: "#FF0043",
     fontWeight: "bold",
   },
-  numbert:{
+  number:{
     fontSize: 48,
     color: "#FF0043",
     fontWeight: "bold",
+  },
+  boxSharedButton: {
+    width: "100%",
+    alignItems: "center",
+    marginBottom: 15,
+  },
+  shared: {
+    backgroundColor: "#1877f2",
+    borderRadius: 50,
+    paddingBottom: 5,
+    paddingTop: 5,
+  },
+  sharedText: {
+    color: "#FFFFFF",
+    fontWeight: "bold",
+    paddingHorizontal: 30,
   },
 });
 
